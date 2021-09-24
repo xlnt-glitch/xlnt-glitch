c353cd552de7b0a55cc7404054956d71623c7e03
encrypted_duck <shiveringdev@icloud.com>
2021-09-24 17:02:56 -0400

    Add project files.

---
+++ encrypted-duck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace encrypted_duck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
